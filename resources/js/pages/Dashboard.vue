6cdd956fa1f4d7ce760e6516c94e7918b3450a3a
dtyronestrand <dtyronestrand@icloud.com>
2026-03-04 19:51:43 -0500

    IDK

---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="flex-container h-full">
+<div class="top-bar flex flex-row">
+<Elbow classList="left-bottom " background="var(--anakiwa)"/>
+<BarWithTitle classList="top" :background="'var(--indigo)'">The Con</BarWithTitle><Bar classList="top" :background="'var(--blue)'"/><BarEnd classList="right decorated top" :background="'var(--indigo)'"/>
+</div>
+<div class="top-content flex flex-row">
+<div class="flex flex-col">
+<Element background="var(--indigo)" :button="false" :height="2"></Element>
+<Element background="var(--periwinkle)" :button="false"></Element>
+<Element background="var(--blue)" :button="false"></Element>
+<Element background="var(--anakiwa)" :button="false"></Element>
+
+<Element background="var(--blue)" :button="false"></Element>
+<Element background="var(--anakiwa)" :button="false"></Element>
+</div>
+<div class="flex flex-row justify-evenly w-full">
+<WeatherWidget />
+<CalendarWidget :isConnected="page.props.isConnected" :events="page.props.events"/>
+</div>
+</div>
+<div class="bottom-bar flex flex-row">
+<Elbow classList="left-top" background="var(--periwinkle)"/>
+<Bar :background="'var(--anakiwa)'"/><BarEnd classList="right decorated" :background="'var(--anakiwa)'"/>
+</div>
+<div class="section-topper flex flex-row">
+<Elbow classList="left-bottom " background="var(--anakiwa)"/>
+<Bar classList="top" :background="'var(--indigo)'"></Bar><Bar classList="top" :background="'var(--blue)'"/><BarEnd classList="right decorated top" :background="'var(--indigo)'"/>
+</div>
+<div class="bottom-content flex flex-row">
+<div class="flex flex-col">
+<Element background="var(--indigo)" :button="true" @buttonPreed="showModal=true">Add Service</Element>
+<Element background="var(--blue)" :button="false"></Element>
+<Element background="var(--periwinkle)" :button="false"></Element>
+<Element background="var(--periwinkle)" :button="false"/>
+<Element background="var(--blue)" :button="false" :height="2"></Element>
+<Element background="var(--anakiwa)" :button="false"></Element>
+<Element background="var(--periwinkle)" :button="false" :height="2"></Element>
+</div>
+<ServiceContainer :categories="page.props.categories" :edit="showModal" @serviceAdded="edit=false"/>
+</div>
+<div class="closer flex flex-row">
+<Elbow classList="left-top" background="var(--periwinkle)"/>
+<Bar :background="'var(--anakiwa)'"/><BarEnd classList="right decorated" :background="'var(--anakiwa)'"/>
+</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Element from '@/components/lcars/Element.vue';
+import WeatherWidget from '@/components/WeatherWidget.vue';
+import CalendarWidget from '@/components/CalendarWidget.vue';
+import Elbow from '@/components/lcars/Elbow.vue';
+  import Bar from '@/components/lcars/Bar.vue';
+import BarEnd from '@/components/lcars/BarEnd.vue';
+import BarWithTitle from '@/components/lcars/BarWithTitle.vue';
+import {usePage} from '@inertiajs/vue3';
+import type { Category, AppPageProps,Event } from '@/types';
+const page = usePage<AppPageProps & {
+    categories: Category[];
+    isConnected: boolean;
+    events: Event[];
+}>();
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/pages/auth/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login flex flex-col gap-8 h-[100dvh] py-32 items-center justify-center">
+    <h1 class="text-5xl text-(--blue) font-bold">Login</h1>
+        <div class="flex w-100 h-100 flex-row gap-4 bg-black rounded-[1.5rem]">
+            <Bracket :classList="['left', 'hollow']" background="var(--blue)" />
+            <Form ref="formRef" action="/login" method="post"
+                class="flex w-full flex-col items-center justify-center gap-4"
+            >
+                <label for="email" class="self-start text-white">Email:</label>
+                <input
+                 name="email"
+                    type="email"
+                    placeholder="Email"
+                    class="w-full rounded border border-gray-300 p-2 text-white"
+                />
+                <label for="password" class="self-start text-white"
+                    >Password:</label>
+                <input
+                 name="password"
+                    type="password"
+                    placeholder="Password"
+                    class="w-full rounded border border-gray-300 p-2 text-white"
+                />
+             <Button @click="handleSubmit" background="var(--blue)" :classList="['round']">Login</Button>
+                  
+            </Form>
+            <Bracket :classList="['right', 'hollow']" background="var(--blue)" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Bracket from '@/components/lcars/Bracket.vue';
+import Button from '@/components/lcars/Button.vue';
+import {Form} from '@inertiajs/vue3';
+import {ref} from 'vue';
+
+const formRef = ref();
+
+const handleSubmit = () => {
+   formRef.value.submit();
+}
+</script>
+
+<style scoped>
+.login {
+    background-image: linear-gradient(in oklch, var(--moonbeam), var(--gray));
+}
+</style>
