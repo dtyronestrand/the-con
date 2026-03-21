0b61ca235597b298bbaf41c0b8d864b60e7a68e5
dtyronestrand <dtyronestrand@icloud.com>
2026-03-21 00:33:54 -0400

    fix: add missing newline at end of eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default defineConfigWithVueTs(
         rules: {
             'vue/block-lang':['error', {
                 allowNoLang: true,
-            }]
+            }],
             'vue/multi-word-component-names': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             'import/order': [
